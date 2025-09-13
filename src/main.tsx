35771d31db15b48ca7dbfb263466e0df7724ece3
riyf20 <rfard13@gmail.com>
2025-09-13 13:43:45 -0400

    Update_1.1.1
    Altered analytics integration in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { Analytics } from '@vercel/analytics/next';
-import '@vercel/analytics';
+import { inject } from '@vercel/analytics';
+inject();
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Analytics />
   </StrictMode>,
 )
