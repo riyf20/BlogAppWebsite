5bc54308e2b3e429cf77f50c18c0c6f79f6d269a
riyf20 <rfard13@gmail.com>
2025-09-13 13:38:33 -0400

    Update_1.1
    Added analytics from Vercel to track website traffic and user interactions.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { Analytics } from '@vercel/analytics/next';
+import '@vercel/analytics';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
